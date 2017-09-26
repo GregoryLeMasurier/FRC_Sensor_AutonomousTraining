84ed540a20d53dabdb05d734825d5d490b144c2a
Gregory LeMasurier <gregorylemasurier@gmail.com>
2017-09-26 17:39:09 -0400

    Fixed format in auto execute

--- src/org/usfirst/frc/team5962/robot/commands/AutoExecute.java
+++ src/org/usfirst/frc/team5962/robot/commands/AutoExecute.java
@@ -7,22 +7,26 @@ public class AutoExecute {
 
 	public void init() {
 		/*
-		 * If you want to move the robot forwards type: driveRobot.forward(speed,
-		 * sensorName, sensorValue); Speed is any number from -1 to 1 sensorName is the
-		 * name of the sensor (encoder or ultrasonic) that you want to use. sensorValue
-		 * is the maximum value you want the sensor to get to while moving forward.
+		 * If you want to move the robot forwards type: 
+		 * driveRobot.forward(speed, sensorName, sensorValue); 
+		 * Speed is any number from -1 to 1 sensorName is the name of the sensor 
+		 * (encoder or ultrasonic) that you want to use. sensorValue is the maximum 
+		 * value you want the sensor to get to while moving forward.
 		 * 
-		 * If you want to move the robot backwards type: driveRobot.backwards(speed,
-		 * sensorName, sensorValue); Speed is any number from -1 to 1 sensorName is the
-		 * name of the sensor (encoder or ultrasonic) that you want to use. sensorValue
-		 * is the maximum value you want the sensor to get to while moving backwards.
+		 * If you want to move the robot backwards type: 
+		 * driveRobot.backwards(speed, sensorName, sensorValue); 
+		 * Speed is any number from -1 to 1 sensorName is the name of the sensor 
+		 * (encoder or ultrasonic) that you want to use. sensorValue is the maximum 
+		 * value you want the sensor to get to while moving backwards.
 		 * 
-		 * If you want to turn the robot to the right type: driveRobot.turn(speed,
-		 * sensorValue); Speed is any number from 0 to 1 (absolute value is taken) and
-		 * sensorValue is the gyroAngle that you want to turn to.
+		 * If you want to turn the robot to the right type: 
+		 * driveRobot.turn(speed, sensorValue); 
+		 * Speed is any number from 0 to 1 (absolute value is taken) and sensorValue 
+		 * is the gyroAngle that you want to turn to.
 		 * 
-		 * If you want to stop the robot type: driveRobot.stop(time); Time is the time
-		 * in seconds that you would like to move forwards.
+		 * If you want to stop the robot type: 
+		 * driveRobot.stop(time); 
+		 * Time is the time in seconds that you would like to move forwards.
 		 * 
 		 * Enter your code below:
 		 */
